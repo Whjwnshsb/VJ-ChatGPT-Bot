21b7ffac0969bbf5bbca66a10c38a20d95499d3d
Whjwnshsb <140898419+Whjwnshsb@users.noreply.github.com>
2023-12-25 18:51:28 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,7 +21,7 @@ class Bot(Client):
         me = await self.get_me()
         self.username = '@' + me.username
             
-        print('Bot Started Powered By @ALAS_NOW)
+        print('Bot Started Powered By @VJ_Botz')
 
 
     async def stop(self, *args):
